0463cfbbdfd1aeecb55e303eb07e5949a32d1872
iit2016092 <iit2016092@iiita.ac.in>
2016-09-12 12:10:00 +0530

    Add files via upload
    Program to check whether a number is prime or not.( After indenting)

--- check_prime.cpp
+++ check_prime.cpp
@@ -1,20 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-int i,n,flag=0;
-printf("\n Enter the number:");
-scanf("%d",&n);
-for(i=2;i<n/2;i++)
-if(n%i==0)
-{
-flag=1;
-break;
+    int i,n,flag=0;
+    printf("\n Enter the number:");
+    scanf("%d",&n);
+    for(i=2;i<n/2;i++)
+    if(n%i==0)
+    {
+        flag=1;
+        break;
+    }
+    if(flag==1)
+    printf("\n The number is not prime");
+    if(flag==0)
+    printf("\n The number is prime");
+    return 0;
+    
 }
-if(flag==1)
-printf("\n The number is not prime");
-else
-printf("\n The number is prime");
-return 0;
-}
-
-
